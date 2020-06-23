01422c9eb059dd2e619d2c80fc34b43680edc864
RafaelM <rafaelm.science@gmail.com>
2020-06-22 20:49:23 -0400

    Feat: DataSource Factory and Repository

---
+++ app/src/main/java/com/rafaelm/moviemvvm/ui/popular_movie/MainActivityViewModel.kt
@@ -0,0 +1,32 @@
+package com.rafaelm.moviemvvm.ui.popular_movie
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
+import com.rafaelm.moviemvvm.data.repository.NetworkState
+import com.rafaelm.moviemvvm.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MainActivityViewModel(
+    private val movieRepository: MoviePageListRepository
+) : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    val moviePagedList: LiveData<PagedList<Movie>> by lazy {
+        movieRepository.fetchLiveMoviePagedList(compositeDisposable)
+    }
+
+    val networkState : LiveData<NetworkState> by lazy {
+        movieRepository.getNetworkState()
+    }
+
+    fun listIsEmpty() : Boolean{
+        return moviePagedList.value?.isEmpty()?:true
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
---
+++ app/src/main/java/com/rafaelm/moviemvvm/ui/popular_movie/MoviePageListRepository.kt
@@ -0,0 +1,38 @@
+package com.rafaelm.moviemvvm.ui.popular_movie
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.rafaelm.moviemvvm.data.api.POST_PER_PAGE
+import com.rafaelm.moviemvvm.data.api.TheMovieDBInterface
+import com.rafaelm.moviemvvm.data.repository.MovieDataSource
+import com.rafaelm.moviemvvm.data.repository.MovieDataSourceFactory
+import com.rafaelm.moviemvvm.data.repository.NetworkState
+import com.rafaelm.moviemvvm.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MoviePageListRepository(private val apiService: TheMovieDBInterface) {
+
+    lateinit var moviePagedList: LiveData<PagedList<Movie>>
+    lateinit var moviesDataSourceFactory: MovieDataSourceFactory
+
+    fun fetchLiveMoviePagedList(compositeDisposable: CompositeDisposable): LiveData<PagedList<Movie>> {
+        moviesDataSourceFactory = MovieDataSourceFactory(apiService, compositeDisposable)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPageSize(POST_PER_PAGE)
+            .build()
+
+        moviePagedList = LivePagedListBuilder(moviesDataSourceFactory, config).build()
+
+        return moviePagedList
+    }
+
+    fun getNetworkState(): LiveData<NetworkState> {
+        return Transformations.switchMap<MovieDataSource, NetworkState>(
+            moviesDataSourceFactory.moviesLiveDataSource, MovieDataSource::networkState
+        )
+    }
+}
